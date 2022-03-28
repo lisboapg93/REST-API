27fe3999b0fac77e19e94e60d68e94396eeb2a85
PedroLisboa <pedro.lisboa@edu.ma.gov.br>
2022-03-28 16:56:16 -0300

    novo

--- config/routes.js
+++ config/routes.js
@@ -1,2 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const routes = express.Router()
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.jason())
+app.use(cors())
+app.use(routes)
+
+
+app.listen(2199, () => {
+    console.log('Express started at http://localhost:2199')
+})
